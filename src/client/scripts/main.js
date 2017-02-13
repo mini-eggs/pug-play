0f45efa8d4332d4f615e0a6730f3f8bc42ffce91
Evan <evan@crowdhubapps.com>
2017-02-13 09:43:36 -0800

    depend

--- src/client/scripts/main.js
+++ src/client/scripts/main.js
@@ -2,10 +2,12 @@
 require('./headerTransitions')
 
 // fade in images
-var backgroundImage = require('../../../../background-image-fade-in/')
+var backgroundImage = require('background-image-fade-in')
 backgroundImage('.featured__image', 250, { 
    backgroundPosition: 'center center',
    backgroundSize: 'cover'
  }, { 
    backgroundColor: 'rgba(0,0,0,0.3)' 
  })
+
+
