6357f97febbcc6821697a9020ae41d5b9e725bbd
Evan <evan@crowdhubapps.com>
2017-02-16 20:50:07 -0800

    heroku

--- shared/reducers/reducers.js
+++ shared/reducers/reducers.js
@@ -2,13 +2,10 @@ import { combineReducers } from 'redux';
 import * as ActionTypes from '../actions/actions';
 
 const contentfulReducer = (state = {}, action) => {
-  console.log(action);
   switch (action.type) {
     case ActionTypes.EXAMPLE_ACTION:
-      console.log(action);
       return state;
     case ActionTypes.CONTENTFUL_INIT:
-      console.log(action);
       return state;
     default:
       return state;
